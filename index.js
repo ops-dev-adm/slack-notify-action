7900524a26fe13725665cd1e24e973c742a76a49
Marcus Mota <marcushcsm@gmail.com>
2020-08-22 17:57:41 +0100

    update commit link

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const moment = require('moment');
 
 (async () => {
     if(process.env.SLACK_DEPLOYMENT_HOOK) {
+
+        const repoLink = `<https://github.com/${process.env.REPOSITORY}/commit/${process.env.SHA}|${process.env.SHA}>`
         const request = {
             url: 'https://hooks.slack.com/services/'+process.env.SLACK_DEPLOYMENT_HOOK, 
             method: 'POST',
-            data: { text: `*New deployment* :rocket:\n\n*App:* ${process.env.REPOSITORY} \n*Branch:* master \n*Commit:* ${process.env.SHA} \n*Environment:* ${process.env.APP_ENV} \n*Date:* ${moment().format('DD/MM/YYYY HH:mm:ss')} \n*Actor:* ${process.env.ACTOR}`},
+            data: { text: `*New deployment* :rocket:\n\n*App:* ${process.env.REPOSITORY} \n*Branch:* master \n*Commit:* ${repoLink} \n*Environment:* ${process.env.APP_ENV} \n*Date:* ${moment().format('DD/MM/YYYY HH:mm:ss')} \n*Actor:* ${process.env.ACTOR}`},
             headers: {
                 'Content-type': 'application/json'
             }
